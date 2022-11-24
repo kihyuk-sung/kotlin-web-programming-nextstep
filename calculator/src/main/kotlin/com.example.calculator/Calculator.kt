d3c8e7381cbce0f19ddf63e04e4adb1e2fd3a738
K <kihyuk.sung@outlook.kr>
2022-11-24 19:48:19 +0900

    feat: Calculator 추가
    - 덧셈 뺄셈 곱셈 나눗셈 가능
    - 메인 코드의 경우 테스트 코드 성격으로 사용
    - 코드를 분리했다고 볼 수 있다.

---
+++ calculator/src/main/kotlin/com.example.calculator/Calculator.kt
@@ -0,0 +1,8 @@
+package com.example.calculator
+
+class Calculator {
+    fun add(i: Int, j: Int): Int = i + j
+    fun subtract(i: Int, j: Int): Int = i - j
+    fun multiply(i: Int, j: Int): Int = i * j
+    fun divide(i: Int, j: Int): Int = i / j
+}
--- calculator/src/main/kotlin/com.example.calculator/CalculatorApp.kt
+++ calculator/src/main/kotlin/com.example.calculator/CalculatorApp.kt
@@ -1,5 +1,10 @@
 package com.example.calculator
 
 fun main() {
-    println("hello world")
+    val cal = Calculator()
+
+    println(cal.add(3, 4))
+    println(cal.subtract(5, 4))
+    println(cal.multiply(2, 6))
+    println(cal.divide(8, 4))
 }
