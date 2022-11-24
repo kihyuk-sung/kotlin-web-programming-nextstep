a18072fa377273b5dfa1a6a5a7825e96d33912ec
K <kihyuk.sung@outlook.kr>
2022-11-24 20:55:48 +0900

    feat: 커스텀 구분자를 지정할 수 있다.

--- calculator/src/main/kotlin/com.example.calculator/StringCalculator.kt
+++ calculator/src/main/kotlin/com.example.calculator/StringCalculator.kt
@@ -1,10 +1,22 @@
 package com.example.calculator
 
 class StringCalculator {
-    fun add(text: String?): Int = text
-        ?.ifEmpty { null }
-        ?.let {
-            it.toIntOrNull() ?: it.split(",", ":").map(String::toInt).sum()
-        }
-        ?: 0
+    companion object {
+        const val COMMA = ","
+        const val COLON = ":"
+        val CUSTOM_DELIMITERS_PATTERN = "//(.)\n(.*)".toRegex()
+    }
+
+    fun add(text: String?): Int {
+        if (text.isNullOrEmpty()) return 0
+
+        val numbers = CUSTOM_DELIMITERS_PATTERN.find(text)
+            ?.let {
+                val (delimiter, str) = it.destructured
+                str.split(delimiter)
+            }
+            ?: text.split(COMMA, COLON)
+
+        return numbers.sumOf(String::toInt)
+    }
 }
