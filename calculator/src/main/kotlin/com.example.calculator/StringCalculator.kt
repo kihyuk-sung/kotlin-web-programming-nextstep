4ec760209f179c7bc1fe52209dbe9ce78fcbaca9
K <kihyuk.sung@outlook.kr>
2022-11-24 20:22:05 +0900

    feat: StringCalculator: 빈 문자열 또는 null이면 0

--- calculator/src/main/kotlin/com.example.calculator/StringCalculator.kt
+++ calculator/src/main/kotlin/com.example.calculator/StringCalculator.kt
@@ -1,5 +1,8 @@
 package com.example.calculator
 
 class StringCalculator {
-    fun add(text: String?): Int = 0
+    fun add(text: String?): Int = text
+        ?.ifEmpty { null }
+        ?.let { 1 }
+        ?: 0
 }
