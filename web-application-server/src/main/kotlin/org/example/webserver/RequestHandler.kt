75fab60897b262b3b760050e11d52687796fa84d
K <kihyuk.sung@outlook.kr>
2022-11-26 11:51:20 +0900

    feat: index.html 응답하기
    - ./webapp에 파일이 있으면 응답
    - 그렇지 않으면 에러 발생

--- web-application-server/src/main/kotlin/org/example/webserver/RequestHandler.kt
+++ web-application-server/src/main/kotlin/org/example/webserver/RequestHandler.kt
@@ -2,9 +2,9 @@ package org.example.webserver
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-import java.io.DataOutputStream
-import java.io.IOException
+import java.io.*
 import java.net.Socket
+import java.nio.file.Files
 
 class RequestHandler(
     private val connection: Socket,
@@ -17,19 +17,56 @@ class RequestHandler(
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.inetAddress, connection.port)
 
         try {
-            connection.getInputStream().use { inputStream ->
-                connection.getOutputStream().use { outputStream ->
-                    val dos = DataOutputStream(outputStream)
-                    val body = "Hello World".toByteArray()
-                    dos.response200Header(body.size)
-                    dos.responseBody(body)
+            BufferedReader(InputStreamReader(connection.getInputStream())).use { reader ->
+                val firstLine: String? = reader.readLine()
+                if (firstLine == null) {
+                    connection.responseHello()
+                    return@use
                 }
+                log.info(firstLine)
+                while (true) {
+                    val line: String = reader.readLine() ?: break
+                    if (line == "") break
+                    log.info(line)
+                }
+                val (method, path) = parseFirstLine(firstLine)
+                if (method == "GET") {
+                    val body = Files.readAllBytes( File("./webapp${path}").toPath())
+                    connection.responseBody(body)
+                    return@use
+                }
+                connection.responseHello()
             }
         } catch (e: IOException) {
+            log.error(e.message)
+        } catch (e: RuntimeException) {
             log.error(e.message)
         }
     }
 
+    private fun parseFirstLine(firstLine: String): Pair<String, String> {
+        val split = firstLine.split(" ")
+        val method = split.getOrNull(0) ?: throw RuntimeException("Method error")
+        val path = split.getOrNull(1) ?: throw RuntimeException("path error")
+        return method to path
+    }
+
+    private fun Socket.responseHello() {
+        getOutputStream().use { outputStream ->
+            val dos = DataOutputStream(outputStream)
+            val body = "Hello World".toByteArray()
+            dos.response200Header(body.size)
+            dos.responseBody(body)
+        }
+    }
+
+    private fun Socket.responseBody(body: ByteArray) {
+        getOutputStream().use { outputStream ->
+            val dos = DataOutputStream(outputStream)
+            dos.response200Header(body.size)
+            dos.responseBody(body)
+        }
+    }
     private fun DataOutputStream.response200Header(lengthOfBodyContent: Int) {
         try {
             writeBytes("HTTP/1.1 200 OK\r\n")
